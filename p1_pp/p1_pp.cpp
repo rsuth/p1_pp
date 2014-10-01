a9edeb92075c26514829aa874a3dabf8a8c158f1
Rick Sutherland <suthe.rick@gmail.com>
2014-10-01 11:55:39 -0700

    initialize board and print

--- p1_pp/p1_pp.cpp
+++ p1_pp/p1_pp.cpp
@@ -7,13 +7,71 @@
 
 using namespace std;
 
+// TITLE:	printMainMenu
+// DESC:	prints the main menu and handles menu selection loop 
+void printMainMenu(bool &quit){
+	string user = "bill";
+	int rows = 10;
+	int** p = initializeBoard(rows);
+	printGaltonBoard(&user,rows,p);
+	hitEnter();
+	quit = true;
+}
 
-void printMainMenu(bool &quit){}
-void printGaltonBoard(const string* username, int* board){}
+// TITLE:	printGaltonBoard
+// DESC:	prints the completed galton board
+//			Takes a pointer to the board state array
+void printGaltonBoard(const string* username, int rows, int** board){
+	for(int i = 0; i < rows; i++){
+		for (int j = 0; j <= i; j++){
+			cout << "    " << *(*(board+j));
+		}
+		cout << "\n";
+	}
+
+}
+
+// TITLE:	printLogo
+// DESC:	prints a Logo
 void printLogo(){}
+
+// TITLE:	printGaltonGame
+// DESC:	prints the completed galton board, also prints 
+//			the users score, takes a pointer to the 
+//			board state array.
 void printGaltonGame(const string* username, int* board){}
+
 void printGameHistory(){}
+
 void signIn(string &username){}
+
 void printIDandStars(){}
-int* runGalton(int balls, int rows){}
-int* runGaltonGame(int balls, int rows){}
+
+// TITLE:	runGalton
+// DESC:	Runs the galton board (displays nothing!)
+//			returns a pointer to the completed board 
+//			state array.
+//int** runGalton(int balls, int rows){}
+
+//int** runGaltonGame(int balls, int rows){}
+
+//int hitAPeg(int probability){}
+
+int** initializeBoard(int rows){
+	
+	int** board = new int*[rows];
+
+	for(int i = 0; i < rows; i++){
+		
+		int* row = new int[i+1];
+		
+		for(int j = 0; j <= i; j++){
+			row[j] = 0;
+		}
+
+		board[i] = row;
+	}
+
+	return board;
+
+}
--- p1_pp/p1_pp.h
+++ p1_pp/p1_pp.h
@@ -3,11 +3,13 @@
 using namespace std;
 
 void printMainMenu(bool &quit);
-void printGaltonBoard(const string* username, int* board);
+void printGaltonBoard(const string* username,int rows, int** board);
 void printLogo();
 void printGaltonGame(const string* username, int* board);
 void printGameHistory();
 void signIn(string &username);
 void printIDandStars();
-int* runGalton(int balls, int rows);
-int* runGaltonGame(int balls, int rows);
+int** runGalton(int balls, int rows);
+int** runGaltonGame(int balls, int rows);
+int** initializeBoard(int rows);
+int hitAPeg();
